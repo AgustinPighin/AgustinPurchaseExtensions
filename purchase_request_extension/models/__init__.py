1f7eed238a57fef837d41d0542ebe3527c8e6345
Agustin Pighin <agustinpighin@gmail.com>
2018-07-10 17:25:55 -0300

    Change folder names

--- purchase_request_extension/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import purchase_request
-from . import procurement
